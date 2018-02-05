34ffad8b8717bad24c7b55a6e1815462ef9105ea
Harikrishna Bokksum <harikrishna.bokksum@mavs.uta.edu>
2018-02-05 15:09:50 -0600

    Exception handling.

---
+++ ServerTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwitterFeed;
+
+namespace ServerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            [TestMethod]
+            public void 
+        }
+    }
+}
--- TwitterFeed/Controllers/Feed.cs
+++ TwitterFeed/Controllers/Feed.cs
@@ -41,103 +41,146 @@ namespace TwitterFeed.Controllers
 
         public string GetTweets(string screenName, int count)
         {
-            var resourceUrl = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+            try
+            {
+                var resourceUrl = "https://api.twitter.com/1.1/statuses/user_timeline.json";
 
-            var response = GetResponse(resourceUrl, Method.GET, screenName, count);            
+                var response = GetResponse(resourceUrl, Method.GET, screenName, count);
 
-            return response;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         private string GetResponse(string resourceUrl, Method method, string screenName, int count)
         {
-            ServicePointManager.Expect100Continue = false;
+            try
+            {
+                ServicePointManager.Expect100Continue = false;
 
-            var authHeader = CreateHeader(resourceUrl, method, screenName, count);
+                var authHeader = CreateHeader(resourceUrl, method, screenName, count);
 
-            var urlParameters = "screen_name=" + Uri.EscapeDataString(screenName) + "&count=" + count;
-            resourceUrl += "?" + urlParameters;
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(resourceUrl);
-            request.Headers.Add("Authorization", authHeader);
-            request.Method = method.ToString();
-            request.ContentType = "application/x-www-form-urlencoded";
-
-
-            WebResponse response = request.GetResponse();
-            string responseData = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                var urlParameters = "screen_name=" + Uri.EscapeDataString(screenName) + "&count=" + count;
+                resourceUrl += "?" + urlParameters;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(resourceUrl);
+                request.Headers.Add("Authorization", authHeader);
+                request.Method = method.ToString();
+                request.ContentType = "application/x-www-form-urlencoded";
 
 
-            return responseData;
+                WebResponse response = request.GetResponse();
+                string responseData = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+
+                return responseData;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         private string CreateHeader(string resourceUrl, Method method, string screenName, int count)
         {
-            var oauthNonce = CreateOauthNonce();
-            var oauthTimestamp = CreateOAuthTimestamp();
-            var oauthSignature = CreateOauthSignature(resourceUrl, method, oauthNonce, oauthTimestamp, screenName, count);
+            try
+            {
+                var oauthNonce = CreateOauthNonce();
+                var oauthTimestamp = CreateOAuthTimestamp();
+                var oauthSignature = CreateOauthSignature(resourceUrl, method, oauthNonce, oauthTimestamp, screenName, count);
 
-            // create the request header
-            var headerFormat = "OAuth oauth_nonce=\"{0}\", oauth_signature_method=\"{1}\", " +
-                               "oauth_timestamp=\"{2}\", oauth_consumer_key=\"{3}\", " +
-                               "oauth_token=\"{4}\", oauth_signature=\"{5}\", " +
-                               "oauth_version=\"{6}\"";
+                // create the request header
+                var headerFormat = "OAuth oauth_nonce=\"{0}\", oauth_signature_method=\"{1}\", " +
+                                   "oauth_timestamp=\"{2}\", oauth_consumer_key=\"{3}\", " +
+                                   "oauth_token=\"{4}\", oauth_signature=\"{5}\", " +
+                                   "oauth_version=\"{6}\"";
 
-            var authHeader = string.Format(headerFormat,
-                                    Uri.EscapeDataString(oauthNonce),
-                                    Uri.EscapeDataString(oauthSignatureMethod),
-                                    Uri.EscapeDataString(oauthTimestamp),
-                                    Uri.EscapeDataString(consumerKey),
-                                    Uri.EscapeDataString(accessToken),
-                                    Uri.EscapeDataString(oauthSignature),
-                                    Uri.EscapeDataString(oauthVersion)
-                            );
+                var authHeader = string.Format(headerFormat,
+                                        Uri.EscapeDataString(oauthNonce),
+                                        Uri.EscapeDataString(oauthSignatureMethod),
+                                        Uri.EscapeDataString(oauthTimestamp),
+                                        Uri.EscapeDataString(consumerKey),
+                                        Uri.EscapeDataString(accessToken),
+                                        Uri.EscapeDataString(oauthSignature),
+                                        Uri.EscapeDataString(oauthVersion)
+                                );
 
 
-            return authHeader;
+                return authHeader;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         private string CreateOauthSignature(string resourceUrl, Method method, string oauthNonce, string oauthTimestamp, string screenName, int count)
         {
-            // create oauth signature
-            var baseFormat = "count={7}&oauth_consumer_key={0}&oauth_nonce={1}&oauth_signature_method={2}" +
-                            "&oauth_timestamp={3}&oauth_token={4}&oauth_version={5}&screen_name={6}";
-
-            var baseString = string.Format(baseFormat,
-                                        consumerKey,
-                                        oauthNonce,
-                                        oauthSignatureMethod,
-                                        oauthTimestamp,
-                                        accessToken,
-                                        oauthVersion,
-                                         Uri.EscapeDataString(screenName),
-                                         Uri.EscapeDataString(count.ToString())
-                                        );
-
-            baseString = string.Concat("GET&", Uri.EscapeDataString(resourceUrl), "&", Uri.EscapeDataString(baseString));
-
-            var compositeKey = string.Concat(Uri.EscapeDataString(consumerKeySecret),
-                                    "&", Uri.EscapeDataString(accessTokenSecret));
-
-            string oauth_signature;
-            using (HMACSHA1 hasher = new HMACSHA1(ASCIIEncoding.ASCII.GetBytes(compositeKey)))
+            try
             {
-                oauth_signature = Convert.ToBase64String(
-                    hasher.ComputeHash(ASCIIEncoding.ASCII.GetBytes(baseString)));
-            }
+                // create oauth signature
+                var baseFormat = "count={7}&oauth_consumer_key={0}&oauth_nonce={1}&oauth_signature_method={2}" +
+                                "&oauth_timestamp={3}&oauth_token={4}&oauth_version={5}&screen_name={6}";
 
-            return oauth_signature;
+                var baseString = string.Format(baseFormat,
+                                            consumerKey,
+                                            oauthNonce,
+                                            oauthSignatureMethod,
+                                            oauthTimestamp,
+                                            accessToken,
+                                            oauthVersion,
+                                             Uri.EscapeDataString(screenName),
+                                             Uri.EscapeDataString(count.ToString())
+                                            );
+
+                baseString = string.Concat("GET&", Uri.EscapeDataString(resourceUrl), "&", Uri.EscapeDataString(baseString));
+
+                var compositeKey = string.Concat(Uri.EscapeDataString(consumerKeySecret),
+                                        "&", Uri.EscapeDataString(accessTokenSecret));
+
+                string oauth_signature;
+                using (HMACSHA1 hasher = new HMACSHA1(ASCIIEncoding.ASCII.GetBytes(compositeKey)))
+                {
+                    oauth_signature = Convert.ToBase64String(
+                        hasher.ComputeHash(ASCIIEncoding.ASCII.GetBytes(baseString)));
+                }
+
+                return oauth_signature;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         private static string CreateOAuthTimestamp()
         {
-            var timeSpan = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            var timestamp = Convert.ToInt64(timeSpan.TotalSeconds).ToString();
+            try
+            {
+                var timeSpan = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                var timestamp = Convert.ToInt64(timeSpan.TotalSeconds).ToString();
 
-            return timestamp;
+                return timestamp;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         private string CreateOauthNonce()
         {
-            return Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
+            try
+            {
+                return Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            
         }
     }
 
